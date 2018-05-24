6f36388192244a41470a56df20431ebdcea7f00f
Chris Hamons <chris.hamons@xamarin.com>
2018-05-24 14:31:43 -0500

    Initial Release

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using QuickType;
+using System.Linq;
+
+namespace emojiDump
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length != 1 || !File.Exists (args[0])) {
+				Console.WriteLine ("mono emojiDump.exe emoji.json");
+				System.Environment.Exit (1);
+			}
+
+			var emoji = Emoji.FromJson (File.ReadAllText (args[0]));
+
+			Console.WriteLine ("static KeyValuePair<char, string>[] cannedPairs = new KeyValuePair<char, string>[] {");
+			foreach (var e in emoji.Where (x => !string.IsNullOrWhiteSpace (x.EmojiEmoji)))
+			{
+				Console.WriteLine ($"new KeyValuePair<char, string> ('{e.EmojiEmoji}', \"{e.Description}\")");
+			}
+			Console.WriteLine ("};");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Emoji Dumper
+
+This is a simple C# script for generating human readable names for emoji, using the data from https://github.com/github/gemoji
